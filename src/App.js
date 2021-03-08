abcb818fc6c031515ff2e2c9bdf12a420b9dc391
Felipe Pimenta <felimenta97@gmail.com>
2021-03-08 10:26:11 -0300

    Added background images

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import Timer from './timeCounter.js';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <Timer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Timer />
+			
+		</div>
+	);
 }
 
 export default App;
